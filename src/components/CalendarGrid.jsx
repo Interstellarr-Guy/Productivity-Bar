c054841fe1056b8a7115cac6797884000475f961
Interstellar-Guy <interstellarguy29@gmail.com>
2026-06-19 21:06:05 +0530

    7 rows added,small ui crt

--- src/components/CalendarGrid.jsx
+++ src/components/CalendarGrid.jsx
@@ -14,15 +14,24 @@ export default function CalendarGrid({ year, month }) {
     ...dates,
   ];
 
-  return (
-    <div className="grid grid-cols-7 h-full">
+    // Update for 7 rows to all days in month
+    const remainingCells = 42 - cells.length;
 
-      {cells.map((cell, index) => (
+const fullCalendar = [
+  ...cells,
+  ...Array(remainingCells).fill(null),
+];
+  return (
+    <div className="grid grid-cols-7 h-full bg-[#777]">
+
+      {fullCalendar.map((cell, index) => (
         <div
           key={index}
-          className="border border-gray-400 relative min-h-30"
+          className={`border border-gray-400 relative min-h-32
+          ${cell === null ? "bg-[#444]" : "bg-[#666]"}
+          `}
         >
-          {cell && (
+          {cell !== null && (
             <span className="absolute top-1 left-1">
               {cell}
             </span>
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,11 +1,31 @@
 import CalendarGrid from "../components/CalendarGrid";
+import { useState } from "react";
 
 export default function Calendar() {
-  const year = 2026;
-  const month = 0; // January
+  const [year] = useState(2026);
+  const [month, setMonth] = useState(0); 
+
+  const months =
+  ["January", "February", "March", "April", "May", "June",
+   "July", "August", "September", "October", "November", "December"];
 
   return (
     <div className="h-screen">
+      <div className="flex gap-2 p-2 bg-[#111]">
+        {
+          months.map((m, index) => (
+            <button
+              key={index}
+              onClick={() => setMonth(index)}
+              className="border px-2 bg-[#cc3b2b] rounded hover:bg-[#2f673d]"
+          >
+            {m}
+          </button>
+          ))
+        }
+
+      </div>
+
       <CalendarGrid
         year={year}
         month={month}
