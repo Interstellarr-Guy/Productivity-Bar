20c9e07d71879ced446cbdba027e2d1749b3b243
Interstellarr-Guy <interstellarguy29@gmail.com>
2026-07-02 11:54:06 +0530

    UI Day cell bar impro

--- src/components/DayCell.jsx
+++ src/components/DayCell.jsx
@@ -4,14 +4,14 @@ export default function DayCell({ day, hours=0, onClick}) {
 
 const fillPercentage = (hours /12) * 100;
     return(
-      <div onClick={onClick} className="border border-[#999] relative min-h-30 bg-[#160202] cursor-pointer rounded ">
+      <div onClick={onClick} className="border border-[#999] relative min-h-30 bg-[#160202]  cursor-pointer rounded ">
       <span className="absolute top-1 left-1 ">
       {day}
       </span>
 
-      <div className="absolute bottom-0 left-0 w-full h-full flex items-end"> 
+      <div className="absolute inset-0 left-0  flex items-end justify-center"> 
         <div
-          className="w-full bg-[#1DB954] rounded-t-xl shadow-xl drop-shadow-xl/50 "
+          className="w-[70%] bg-[#13b24b] rounded-t-xl shadow-xl drop-shadow-xl/50 "
           style={{
             height: `${fillPercentage}%`
           }}
