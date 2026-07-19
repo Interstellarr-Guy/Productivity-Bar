4e9df00e71ae7638cb86bfba928349bd952f2b6a
Interstellar-Guy <interstellarguy29@gmail.com>
2026-07-19 09:25:42 +0530

    url fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "productivity-tracker-api-production.up.railway.app",
+  baseURL: "http://productivity-tracker-api-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
