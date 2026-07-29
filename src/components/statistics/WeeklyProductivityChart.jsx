4ac9a17ae6d046cd7dba4b39db5d95b1dd723a14
Interstellar-Guy <interstellarguy29@gmail.com>
2026-07-29 18:37:25 +0530

    chart added

---
+++ src/components/statistics/WeeklyProductivityChart.jsx
@@ -0,0 +1,69 @@
+import {
+    BarChart,Bar,
+    XAxis,YAxis,
+    Tooltip,ResponsiveContainer, CartesianGrid} from "recharts";
+
+export default function WeeklyProductivityChart({ weeklyData}) {
+    
+    console.log(weeklyData);
+
+    return (
+
+        <div className="bg-[#1f2937] rounded-xl p-5 shadow-lg border
+        border-gray-700">
+
+            <h2 className="text-white text-lg font-semibold mb-4">
+                📊 Weekly Productivity
+            </h2>
+
+            <ResponsiveContainer width="100%" height={300}>
+
+                <BarChart data={weeklyData}
+                margin={{
+                top: 20,
+                right: 200,
+                left: 100,
+                bottom: 10,
+    }}
+    >
+                    <CartesianGrid
+                        strokeDasharray="3 3"
+                        stroke="#374151"
+                    />
+                    <XAxis dataKey="day"
+                    tick={{ fill: "#ffffff" }} 
+                    />
+
+                    <YAxis 
+                    tick={{ fill: "#ffffff" }}
+                    />
+
+                    <Tooltip
+                    cursor={{ fill: "transparent" }}
+                    contentStyle={{
+                    backgroundColor: "#1f2937",
+                    border: "none",
+                    borderRadius: "8px",
+                    color: "white",
+    }}
+/>
+
+                    <Bar
+                        dataKey="minutes"
+                        fill="#22c55e"
+                        radius={[6, 6, 0, 0]}
+                        barSize={65}
+                    />
+
+                    
+
+                </BarChart>
+  
+
+            </ResponsiveContainer>
+
+        </div>
+
+    );
+
+}
--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,12 +1,42 @@
-export default function Analytics() {
+import WeeklyProductivityChart from "../components/statistics/WeeklyProductivityChart";
+import { useEffect, useState } from "react";
+import statisticsService from "../services/statisticsService";
 
+export default function Analytics() {
+   
+    const [weeklyData, setWeeklyData] = useState([]);
+
+    useEffect(() => {
+
+    loadWeeklyData();
+
+}, []);
+    
+const loadWeeklyData = async () => {
+
+    try {
+
+        const data =
+            await statisticsService.getWeeklyProductivity();
+
+        setWeeklyData(data);
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+};
     return (
 
-        <div className="text-white text-3xl p-10">
+        <div className="flex flex-col gap-4">
 
-            📊 Analytics Page
+        <WeeklyProductivityChart
+           weeklyData={weeklyData}
+         />
 
-        </div>
+    </div>
 
     );
 
